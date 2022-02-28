3d62740b6c6734da51a9a85de69b7ca08cb175e0
Mrad-Hamza <hamza.mrad@esprit.tn>
2022-02-28 17:46:10 +0100

    first commit

---
+++ backend/routes/users.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const mailAddress = req.body.mailAddress;
+  const password = req.body.password;
+
+  const newUser = new User({firstname,lastname,mailAddress,password});
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').put((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.firstname = req.body.firstname;
+      user.lastname = req.body.lastname;
+      user.mailAddress = req.body.mailAddress;
+      user.password = req.body.password;
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+module.exports = router;
