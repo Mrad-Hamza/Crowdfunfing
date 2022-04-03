d87eea421c8a3faa9655f4cfdfdd5c655fa581b6
MohsniNour <nourelhouda.mohsni@esprit.tn>
2022-04-03 15:50:34 +0100

    add upload image to project and fix list

--- src/pages/Projects/projectComponent.js
+++ src/pages/Projects/projectComponent.js
@@ -8,17 +8,18 @@ import logo from "../../assets/layout/images/project-logo.png";
 const ProjectComponent = () => {
     const projects = useSelector((state) => state.projects.projects);
     const renderList = projects.map((project) => {
-        const { _id, projectName, projectDescription, projectCollectedAmount } = project;
+        const { _id, projectName, projectDescription, projectCollectedAmount, image } = project;
         return (
             <div className="col-12 md:col-2 lg:col-3" key={_id}>
                 <div className="card m-3 border-1 surface-border">
-                    <div className="flex align-items-center justify-content-between">
-                       </div>
+                    <div className="flex align-items-center justify-content-between"></div>
                     <div className="text-center">
                         <img className="w-9 shadow-2 my-3 mx-0" src={logo} />
-                         <div className="text-xl font-bold mb-3"> {projectName}</div>
+                        <div className="text-xl font-bold mb-3"> {projectName}</div>
                         <Link to={`/projects/${_id}`}>
-                            <div className="text font-semibold">{projectDescription} : {projectCollectedAmount}DT</div>
+                            <div className="text font-semibold">
+                                {projectDescription} : {projectCollectedAmount}DT
+                            </div>
                         </Link>
                     </div>
                     {/* <div className="flex align-items-center justify-content-between">
